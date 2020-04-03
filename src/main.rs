58cdf94fc3f2c028a397af80fb57b2904a5d37de
吴翱翔 <pylint@yandex.com>
2020-04-03 14:25:04 +0800

    编写了第一个rust单元测试用例

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,13 @@
+// mod tests;
 
 fn main() {
-    println!("Hello, world!");
+  println!("Hello, world!");
+  // tests::add
+}
+
+mod some_tests {
+  #[test]
+  fn test_1() {
+    assert_eq!(1, 2);
+  }
 }
