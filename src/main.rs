fa6015c511c87afe80951384195e7ec2815c8e65
吴翱翔 <pylint@yandex.com>
2020-04-03 19:40:10 +0800

    学习了tuple和random

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,20 @@
-use std::io;
+extern crate rand;
+use rand::Rng;
 
 fn main() {
-  let mut input = String::new();
-  io::stdin().read_line(&mut input)
-    .ok()
-    .expect("Error in read");
-  println!("input is {}", input);
+  let tuple = (0,1.1,(false, "1"));
+  println!("{}", (tuple.2).1);
+  let (a, b) = tuple.2;
+  println!("a={}, b={}", a, b);
+  let random_number : u8 = rand::thread_rng().gen_range(1, 101);
+  println!("{}", random_number);
+  match 50.cmp(&random_number) {
+    std::cmp::Ordering::Less    => println!("Too small!"),
+    std::cmp::Ordering::Greater => println!("Too big!"),
+    std::cmp::Ordering::Equal   => println!("You win!"),
+  }
+  // thread_rng() function to get a copy of the random number generator, which is local to the particular thread of execution we’re in
+  // Because we use rand::Rng’d above, it has a gen_range() method available
 }
 
 // u8指的是uint8
