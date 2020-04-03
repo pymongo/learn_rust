48f1fd3cfced271996c2eb260e2f0b64da39247d
吴翱翔 <pylint@yandex.com>
2020-04-03 14:36:35 +0800

    添加了panic的测试用例

--- src/main.rs
+++ src/main.rs
@@ -5,9 +5,17 @@ fn main() {
   // tests::add
 }
 
+#[cfg(test)] // 仅在测试环境下编译
 mod some_tests {
   #[test]
   fn test_1() {
-    assert_eq!(1, 2);
+    assert_eq!(1, 1);
+    assert_ne!(1, 2);
+  }
+
+  #[test]
+  // #[should_panic] 会pass掉panic抛异常的测试用例
+  fn test_2() {
+    panic!("throw/raise my exception");
   }
 }
