19de2478e9fe7f5fec6fd076062024f8430dd90f
吴翱翔 <pylint@yandex.com>
2020-05-11 15:52:27 +0800

    添加了Rust内嵌汇编代码的例子

---
+++ examples/inline_assembly.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+
+int main() {
+    asm("NOP");
+    printf("after NOP");
+    return 0;
+}
---
+++ examples/inline_assembly.rs
@@ -0,0 +1,9 @@
+#![feature(asm)]
+// cargo +nightly run --example inline_assembly
+fn main() {
+  unsafe {
+    // asm!("NOP");
+    llvm_asm!("NOP");
+  }
+  println!("after NOP");
+}
