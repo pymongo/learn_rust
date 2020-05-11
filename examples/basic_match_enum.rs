9bdd3c778337c67efc428ba258a84163909e2491
吴翱翔 <pylint@yandex.com>
2020-05-11 17:12:48 +0800

    将部分文件移到了examples中

---
+++ examples/basic_match_enum.rs
@@ -0,0 +1,19 @@
+enum Gender {
+  Male,
+  #[allow(dead_code)]
+  Female
+}
+
+impl Gender {
+  fn is_male(&self) -> bool {
+    match self {
+      &Gender::Male => true,
+      &Gender::Female => false,
+    }
+  }
+}
+
+pub fn main() {
+  let gender: Gender = Gender::Male;
+  println!("gender.is_male() = {}", gender.is_male());
+}
--- src/learn/mod.rs
+++ src/learn/mod.rs
@@ -1,11 +1,7 @@
 // 如果module是在另一个文件夹内，这个文件夹内必须有一个mod.rs
 // 如果是在main.rs同级文件夹中，直接mod 文件名;即可
-pub mod argv;
-pub mod enum_match;
 pub mod iter_vec;
 //pub mod http_request;
 pub mod random_and_tuple;
-pub mod read_file;
 pub mod reference;
-pub mod std_input;
 pub mod struct_and_impl;
