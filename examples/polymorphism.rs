c99be2719f94aa216967425ae21a7d58b2a2cc51
吴翱翔 <pylint@yandex.com>
2020-09-02 15:29:26 +0800

    完成了Rust多态的Demo

--- examples/polymorphism.rs
+++ examples/polymorphism.rs
@@ -1,3 +1,6 @@
+// Any主要是用于多态时upcast，但是Rust多态不需要向上塑型，而且我代码里没有管Any
+// `trait A: B`也可以写成`trait A where Self: B`
+// Rust没有继承, trait A : B 实际上是给A加上一个约束条件: implement B also need to implement A
 trait Animal {
     fn eat(&self);
 }
@@ -18,8 +21,18 @@ impl Animal for Dog {
     }
 }
 
+fn make_animal_eating(animal: &dyn Animal) {
+    animal.eat();
+}
+
 fn main() {
-    let cat = Cat{};
-    let dog = Dog{};
-    let animals = vec![cat, dog];
+    make_animal_eating(&Cat{});
+    make_animal_eating(&Dog{});
+    // cat和dog实例需要分配在堆内存中才能装入Vec，否则会报错: Sized is not known at compile time
+    let cat = Box::new(Cat{});
+    let dog = Box::new(Dog{});
+    let animals: Vec<Box<dyn Animal>> = vec![cat, dog];
+    for animal in animals {
+        animal.eat();
+    }
 }
