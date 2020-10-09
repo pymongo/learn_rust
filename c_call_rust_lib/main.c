b5f917a2bd9393ca6ee92cdd608d0ff80e955b95
wuaoxiang <pylint@yandex.com>
2020-10-09 15:48:26 +0800

    Add C get_string from rust example

--- c_call_rust_lib/main.c
+++ c_call_rust_lib/main.c
@@ -1,12 +1,20 @@
+#include <stdio.h>
 void p();
 void hello(const char *str);
+char *repeat_hi(int times);
+void free_cstr(const char *str);
 
 /*
-static  link: gcc main.c ../target/debug/libc_call_rust_lib.a
-dynamic link: gcc main.c -Isrc -L ../target/debug/ -lc_call_rust_lib
+static  link:
+gcc main.c ../target/debug/libc_call_rust_lib.a
+dynamic link:
+gcc main.c -Isrc -L ../target/debug/ -lc_call_rust_lib
 */
 int main() {
     p();
     hello("rust");
+    char* str = repeat_hi(3);
+    printf("%s", str);
+    free_cstr(str);
     return 0;
 }
--- c_call_rust_lib/src/lib.rs
+++ c_call_rust_lib/src/lib.rs
@@ -1,5 +1,5 @@
 use std::os::raw::{c_char, c_int};
-use std::ffi::CStr;
+use std::ffi::{CStr, CString};
 
 #[no_mangle]
 pub extern "C" fn p() {
@@ -8,6 +8,7 @@ pub extern "C" fn p() {
 
 #[no_mangle]
 pub extern "C" fn hello(name: *const c_char) {
+    assert!(!name.is_null());
     let c_str = unsafe {
         CStr::from_ptr(name)
     };
@@ -15,4 +16,19 @@ pub extern "C" fn hello(name: *const c_char) {
 }
 
 #[no_mangle]
-pub extern "C" fn repeat_hi()
+pub extern "C" fn repeat_hi(times: c_int) -> *mut c_char {
+    let mut res = String::new();
+    res.extend(std::iter::repeat("hi").take(times as usize));
+    CString::new(res).unwrap().into_raw()
+}
+
+#[no_mangle]
+pub extern "C" fn free_cstr(str: *mut c_char) {
+    // Rust的CStr交给C语言管理后，由于用的是堆内存，C语言并不会自动释放，所以还需要提供一个API去释放堆内存
+    if str.is_null() {
+        return;
+    }
+    unsafe {
+        CString::from_raw(str);
+    };
+}
