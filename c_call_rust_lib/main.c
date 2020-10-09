0b673d57a96f6196c2bf6df024dfb8ed5c631a18
wuaoxiang <pylint@yandex.com>
2020-10-09 16:20:28 +0800

    use dbg! to print struct from c

--- c_call_rust_lib/main.c
+++ c_call_rust_lib/main.c
@@ -3,10 +3,18 @@ void p();
 void hello(const char *str);
 char *repeat_hi(int times);
 void free_cstr(const char *str);
+int sum_of_positive(int *nums, size_t len);
+
+typedef struct {
+    int x;
+    int y;
+} Point;
+
+void print_point(Point point);
 
 /*
 static  link:
-gcc main.c ../target/debug/libc_call_rust_lib.a
+cargo build && gcc main.c ../target/debug/libc_call_rust_lib.a && ./a.out
 dynamic link:
 gcc main.c -Isrc -L ../target/debug/ -lc_call_rust_lib
 */
@@ -14,7 +22,12 @@ int main() {
     p();
     hello("rust");
     char* str = repeat_hi(3);
-    printf("%s", str);
+    printf("%s\n", str);
     free_cstr(str);
+    int nums[3] = {1, -1, 3};
+    int sum = sum_of_positive(nums, 3);
+    printf("sum = %d\n", sum);
+    Point point = {.x=1, .y=2};
+    print_point(point);
     return 0;
 }
--- c_call_rust_lib/src/lib.rs
+++ c_call_rust_lib/src/lib.rs
@@ -1,4 +1,4 @@
-use std::os::raw::{c_char, c_int};
+use std::os::raw::{c_char, c_int, c_uint};
 use std::ffi::{CStr, CString};
 
 #[no_mangle]
@@ -24,11 +24,35 @@ pub extern "C" fn repeat_hi(times: c_int) -> *mut c_char {
 
 #[no_mangle]
 pub extern "C" fn free_cstr(str: *mut c_char) {
-    // Rust的CStr交给C语言管理后，由于用的是堆内存，C语言并不会自动释放，所以还需要提供一个API去释放堆内存
+    // 在Rust函数内创建的堆内存变量，C语言并不会自动释放，所以还需要在Rust侧提供一个API去释放堆内存
     if str.is_null() {
         return;
     }
     unsafe {
         CString::from_raw(str);
     };
+}
+
+#[no_mangle]
+pub extern "C" fn sum_of_positive(arr: *const c_int, len: c_uint) -> c_int {
+    if arr.is_null() {
+        return c_int::from(-1);
+    }
+    let nums = unsafe {
+        std::slice::from_raw_parts(arr, len as usize)
+    };
+    let sum: i32 = nums.iter().filter(|&x| x.is_positive()).sum();
+    c_int::from(sum)
+}
+
+#[derive(Debug)]
+#[repr(C)]
+pub struct Point {
+    x: i32,
+    y: i32,
+}
+
+#[no_mangle]
+pub extern "C" fn print_point(point: Point) {
+    dbg!(point);
 }
