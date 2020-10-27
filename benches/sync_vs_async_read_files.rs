55ff60332ccb3b0aa64abf3581b480ee57d36a9d
wuaoxiang <pylint@yandex.com>
2020-10-27 13:03:37 +0800

    add tokio async benchmark with sync

--- benches/sync_vs_async_read_files.rs
+++ benches/sync_vs_async_read_files.rs
@@ -1,17 +1,18 @@
-/*!
-test bench_async_read_files ... bench:      95,751 ns/iter (+/- 4,091)
-test bench_sync_read_files  ... bench:     214,285 ns/iter (+/- 32,424)
+/*! tokio的M(os thread)/N(green thread)的性能碾压futures
+test bench_async_async_std                      ... bench:     104,534 ns/iter (+/- 53,638)
+test bench_async_futures_single_thread_executor ... bench:     112,197 ns/iter (+/- 53,057)
+test bench_async_tokio_multi_threads            ... bench:         620 ns/iter (+/- 7,581)
+test bench_sync                                 ... bench:   1,069,225 ns/iter (+/- 4,256,327)
 */
 #![feature(test)]
 extern crate test;
-use itertools::Itertools;
 use std::io::Read;
 
 const FILE_PATH: &str = "Cargo.toml";
 const READ_TIMES: usize = 10;
 
 #[bench]
-fn bench_sync_read_files(bencher: &mut test::Bencher) {
+fn bench_sync(bencher: &mut test::Bencher) {
     bencher.iter(|| {
         let thread_handlers = (0..READ_TIMES)
             .map(|_| {
@@ -20,21 +21,68 @@ fn bench_sync_read_files(bencher: &mut test::Bencher) {
                     file.read_to_string(&mut String::new()).unwrap();
                 })
             })
-            .collect_vec();
+            .collect::<Vec<_>>();
         for handler in thread_handlers {
             handler.join().unwrap();
         }
     });
 }
 
+async fn read_files() {
+    futures::future::join_all(
+        (0..READ_TIMES).map(|_| async_std::fs::read_to_string(FILE_PATH)),
+    ).await;
+}
+
 #[bench]
-fn bench_async_read_files(bencher: &mut test::Bencher) {
-    bencher.iter(|| {
-        futures::executor::block_on(async {
-            futures::future::join_all(
-                (0..READ_TIMES).map(|_| async_std::fs::read_to_string(FILE_PATH)),
-            )
+fn bench_async_futures_single_thread_executor(bencher: &mut test::Bencher) {
+    bencher.iter(|| futures::executor::block_on(read_files()));
+}
+
+#[bench]
+fn bench_async_tokio_multi_threads(bencher: &mut test::Bencher) {
+    let rt = tokio::runtime::Runtime::new().unwrap();
+    bencher.iter(|| rt.spawn(read_files()));
+}
+
+#[bench]
+fn bench_async_async_std(bencher: &mut test::Bencher) {
+    bencher.iter(|| async_std::task::block_on(read_files()));
+}
+
+
+#[bench]
+#[ignore]
+fn bench_async_futures_multi_threads_executor(bencher: &mut test::Bencher) {
+    // FIXME would stuck
+    let pool = futures::executor::ThreadPool::new().unwrap();
+    bencher.iter(|| pool.spawn_ok(read_files()));
+}
+
+/**
+futures::executor::ThreadPool: 64.484µs
+tokio::runtime::Runtime: 542.913µs
+*/
+// #[test]
+#[cfg(FALSE)]
+fn test_async_multi_threads_executor() {
+    let pool = futures::executor::ThreadPool::new().unwrap();
+    let now = std::time::Instant::now();
+    pool.spawn_ok(async {
+        futures::future::join_all(
+            (0..READ_TIMES).map(|_| async_std::fs::read_to_string(FILE_PATH)),
+        )
             .await;
-        });
     });
+    println!("futures::executor::ThreadPool: {:?}", now.elapsed());
+    drop(pool);
+
+    let rt = tokio::runtime::Runtime::new().unwrap();
+    rt.spawn(async {
+        futures::future::join_all(
+            (0..READ_TIMES).map(|_| async_std::fs::read_to_string(FILE_PATH)),
+        )
+            .await;
+    });
+    println!("tokio::runtime::Runtime: {:?}", now.elapsed());
 }
